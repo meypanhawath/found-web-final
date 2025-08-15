386033c6362d45cf86de0fbe1ea3e43544386fa8
meypanhawath <panhawath18@gmail.com>
2025-08-16 00:51:25 +0700

    mock structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <p className='text-7xl text-center font-medium mt-20'>Testing</p>
+    </>
+  )
+}
+
+export default App
