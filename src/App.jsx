92c17f970dd1623374794cff38f0de2ec29cc562
Theounseyha <theounseyha199@gmail.com>
2025-09-09 21:12:04 +0700

    pull image from yanut

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import About from "./pages/about/About";
+import Support from "./pages/support/Support";
+import Template from "./pages/template/Template";
 
 function App() {
   return (
@@ -14,6 +17,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/templates" element={<Template />} />
+          {/* Optionally, add a 404 Not Found route */}
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </div>
     </Router>
